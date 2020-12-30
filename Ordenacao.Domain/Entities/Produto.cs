35ee3e15581c713f18c23d98150ca68d42a414be
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-12-30 14:40:20 -0300

    Domain: 'Equals' override for 'Produto'

--- Ordenacao.Domain/Entities/Produto.cs
+++ Ordenacao.Domain/Entities/Produto.cs
@@ -12,5 +12,15 @@ namespace Ordenacao.Domain.Entities
 
     public string Nome { get; set; }
     public decimal Preco { get; set; }
+
+    public override bool Equals(object obj)
+    {
+      if (!(obj is Produto))
+        return false;
+
+      var produto = (Produto)obj;
+
+      return Nome == produto.Nome && Preco == produto.Preco;
+    }
   }
 }
