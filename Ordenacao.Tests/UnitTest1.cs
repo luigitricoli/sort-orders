f2ccd59d9fff33275b88d2777d7942ce4d4321b9
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-12-30 14:40:40 -0300

    Tests: All tests passing

---
+++ Ordenacao.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ordenacao.Tests
+{
+    public class UnitTest1
+    {
+        public void TestMethod1()
+        {
+        }
+    }
+}
